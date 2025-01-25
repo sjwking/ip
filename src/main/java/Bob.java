f387464005d00a37105047a74a0d2737f95b3ca3
sjwking <kingsleyser@gmail.com>
2025-01-26 01:35:22 +0800

    Renamed to Bob and added hello and goodbye greeting

---
+++ src/main/java/Bob.java
@@ -0,0 +1,22 @@
+public class Bob {
+    public static void main(String[] args) {
+
+        String logo = " _____             _ \n"
+                + "|  _  \\           | |\n"
+                + "| |_|  |   ____   | |\n"
+                + "|     /   / __ \\  | |___\n"
+                + "|  _  \\  | |  | | |  __ \\\n"
+                + "| |_|  | | |__| | | |__| |\n"
+                + "|_____/   \\____/  |_____/\n";
+            
+
+        System.out.println(logo);
+        System.out.println("_________________________________________");
+        System.out.println("Hello! I'm Bob!");
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________________");
+
+    }
+}
