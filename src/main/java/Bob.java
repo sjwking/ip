8617343e1371e7a9e652d5de0c696b3378d0f44d
sjwking <kingsleyser@gmail.com>
2025-01-31 14:50:06 +0800

    Added echo command and program exits when user inputs "bye"

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
 
@@ -15,8 +17,28 @@ public class Bob {
         System.out.println("Hello! I'm Bob!");
         System.out.println("What can I do for you?");
         System.out.println("_________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_________________________________________");
+
+
+        boolean mainFlag = true;
+        while(mainFlag) {
+            System.out.println("Enter input: ");
+            Scanner input = new Scanner(System.in);
+            String line = input.nextLine();
+
+            System.out.println("_________________________________________");
+
+            if(line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                mainFlag = false;
+            }
+            else {
+                System.out.println(line);
+            }
+
+            System.out.println("_________________________________________");
+        }
 
     }
+
+   
 }
